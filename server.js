2686e05d37d265e37dad27d9eaa9acc3f1dad0d1
Taihlor Elliott <taihlor.elliott@gmail.com>
2024-11-18 14:48:10 -0500

    edit video

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ dotenv.config(); //loads the variables from .env
 const express = require('express');
 // require mongoose package to work
 const mongoose = require('mongoose');
+// require the method-override package to work
+const methodOverride = require("method-override");
+// require the morgan package to work
+const morgan = require('morgan');
 
 const app = express();
 
@@ -16,6 +20,14 @@ mongoose.connection.on('connected', () => {
     console.log(`connected to MongoDB ${mongoose.connection.name}`)
 })
 
+//this tells express "hey expect user data to come in from a form"
+app.use(express.urlencoded({ extended: false }));
+//the string in this will be a query param
+app.use(methodOverride("_method"));
+//helps with finding bugs sometimes, not 100% necessary
+// app.use(morgan("dev"));
+
+
 //importing the fruit model (friut.js)
 const Fruit = require("./models/fruit.js");
 
@@ -23,9 +35,51 @@ app.get('/', async (req, res) => {
     res.render("index.ejs")
 })
 
+//building a route to the edit page
+app.get("/fruits/:fruitId/edit", async (req, res)=>{
+    const foundFruit = await Fruit.findById(req.params.fruitId);
+    res.render('fruits/edit.ejs', {fruit: foundFruit})
+})
+
 //building new fruits page
 app.get('/fruits/new', (req, res) => {
-    res.send("This route sends the user a form page!");
+    //res.render to show the ejs page
+    res.render("fruits/new.ejs");
+})
+
+//defining a show route for the individual fruits has to go after 
+//fruit/new so when you route to fruit/new this does not override it
+app.get('/fruits/:fruitId', async (req, res) => {
+    const foundFruit = await Fruit.findById(req.params.fruitId);
+    res.render("fruits/show.ejs", {
+        fruit: foundFruit
+    })
+});
+
+//post fruits
+app.post("/fruits", async (req, res) => {
+    if (req.body.isReadyToEat === "on") {
+      req.body.isReadyToEat = true;
+    } else {
+      req.body.isReadyToEat = false;
+    }
+    await Fruit.create(req.body);
+    // take me to another page
+    res.redirect('/fruits');
+})
+
+//establish and build the delete route
+app.delete("/fruits/:fruitId", async (req,res) => {
+    await Fruit.findByIdAndDelete(req.params.fruitId)
+    res.redirect('/fruits')
+})
+
+//get fruits to all display
+app.get("/fruits", async (req, res)=> {
+    const allFruits = await Fruit.find()
+    res.render("fruits/index.ejs", {
+        fruits: allFruits
+    })
 })
 
 app.listen(3001, () => {
